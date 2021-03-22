a888dfa837218d88ba2dd95ab26ee8af863eec76
Piyush Purang <ppurang@gmail.com>
2021-03-22 21:58:41 +0100

    use blocking instead of delay for io - better handling of finite duration - fix deprecated usage

--- src/main/scala/org/purang/net/http/asynchttpclient/AsyncHttpClient.scala
+++ src/main/scala/org/purang/net/http/asynchttpclient/AsyncHttpClient.scala
@@ -1,27 +1,26 @@
 package org.purang.net.http.asynchttpclient
 
-import org.asynchttpclient.{Request => _, Response => AResponse, _}
+import org.asynchttpclient.{Response => AResponse, Request => _, _}
 import org.asynchttpclient.{AsyncHttpClient => UnderlyingClient}
 import org.asynchttpclient.{AsyncHandler => Handler}
 import io.netty.handler.codec.http.HttpHeaders
-
 import cats.Defer
 import cats.data.NonEmptyChain
 import cats.effect.Sync
 import cats.effect.Async
 import cats.effect.Deferred
 import cats.effect.Concurrent
-
 import cats.syntax.flatMap._
 import cats.syntax.functor._
-
 import org.purang.net.http._
 import Constants.UTF8
+
+import scala.concurrent.duration.FiniteDuration
 
 object AsyncHttpClient {
 
   val responseToResponse : AResponse => HttpResponse = response => {
-    import scala.collection.JavaConverters._
+    import scala.jdk.CollectionConverters._
     val headers: Vector[Header] =
       response.getHeaders.asScala.groupBy(_.getKey).foldLeft(Vector[Header]()) { case (hdrs, (key, iterable)) =>
         hdrs :+ (Header(key,
@@ -41,7 +40,7 @@ object AsyncHttpClient {
   // Do we really need to delay creation of HttpClient ... would HttpClient[F] (instead of F[HttpClient[F]]) be enough?
   def sync[F[+_]](client: => UnderlyingClient)(implicit S: Sync[F]): F[HttpClient[F]] = S.delay {
     new HttpClient[F] {
-      def execute(req: HttpRequest, timeout: Timeout): F[HttpResponse] = S.delay {
+      def execute(req: HttpRequest, timeout: Timeout): F[HttpResponse] = S.blocking {
         val builder: RequestBuilder = new RequestBuilder(req.method.toString).setUrl(req.url.url)
         for {
           headers <- req.headers
@@ -71,6 +70,7 @@ object AsyncHttpClient {
     new HttpClient[F] {
       def execute(req: HttpRequest, timeout: Timeout): F[HttpResponse] = {
         for {
+          ft <- A.delay(FiniteDuration(timeout.length, timeout.unit))
           aresponse <- A.timeout(A.async_[AResponse]{ cb =>
             val builder: RequestBuilder = new RequestBuilder(req.method.toString).setUrl(req.url.url)
             for {
@@ -90,7 +90,7 @@ object AsyncHttpClient {
               builder.setBody(content.b.getBytes(UTF8))
             }
             client.executeRequest(builder, CustomHandler(cb))
-          }, timeout.finiteDuration)
+          }, ft)
         } yield {
           responseToResponse(aresponse)
         }
