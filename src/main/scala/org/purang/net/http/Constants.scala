c743281ba1858e47ada63780d50319e423a32117
Scala Steward <scala_steward@virtuslab.com>
2026-06-18 21:05:42 +0000

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- src/main/scala/org/purang/net/http/Constants.scala
+++ src/main/scala/org/purang/net/http/Constants.scala
@@ -3,6 +3,6 @@ package org.purang.net.http
 import java.nio.charset.Charset
 
 object Constants {
-  val CRLF: String = "\r\n"
+  val CRLF: String  = "\r\n"
   val UTF8: Charset = java.nio.charset.StandardCharsets.UTF_8
 }
